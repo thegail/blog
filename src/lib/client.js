18fba809a9c799ee3677bb9017a041f46cde63cc
Teddy Gaillard <teddy.gaillard@icloud.com>
2026-01-20 23:17:34 -0800

    Switch Mongo client to use Atlas

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,8 +1,10 @@
 import { MongoClient } from "mongodb";
 
-// let client = new MongoClient(process.env.MONGODB_URI, {
-//   appName: "devrel.vercel.integration",
-//   maxIdleTimeMS: 5000,
-// });
-let client = new MongoClient("mongodb://localhost");
+let client = new MongoClient(process.env.MONGODB_URI, {
+  appName: "devrel.vercel.integration",
+  maxIdleTimeMS: 5000,
+});
+
+// let client = new MongoClient("mongodb://localhost");
+
 export default client;
